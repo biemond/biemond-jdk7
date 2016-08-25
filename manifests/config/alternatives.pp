8ffcab41aa94c527c8d448df33135d88c40bd17e
Stephen Olander-Waters <(none)>
2016-08-25 16:51:03 -0500

    Support java_sdk alternative

--- manifests/config/alternatives.pp
+++ manifests/config/alternatives.pp
@@ -21,12 +21,24 @@ define jdk7::config::alternatives(
     }
   }
 
-  exec { "java alternatives ${title}":
-    command   => "${alt_command} --install /usr/bin/${title} ${title} ${java_home_dir}/${full_version}/bin/${title} ${priority}",
-    unless    => "${alt_command} --display ${title} | /bin/grep ${full_version} | /bin/grep 'priority ${priority}$'",
-    path      => '/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin',
-    logoutput => true,
-    user      => $user,
-    group     => $group,
+  if $title == "java_sdk" {
+    exec { "java alternatives ${title}":
+      command   => "${alt_command} --install /etc/alternatives/{title} ${title} ${java_home_dir}/${full_version} ${priority}",
+      unless    => "${alt_command} --display ${title} | /bin/grep ${full_version} | /bin/grep 'priority ${priority}$'",
+      path      => '/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin',
+      logoutput => true,
+      user      => $user,
+      group     => $group,
+    }
+  }
+  else {
+    exec { "java alternatives ${title}":
+      command   => "${alt_command} --install /usr/bin/${title} ${title} ${java_home_dir}/${full_version}/bin/${title} ${priority}",
+      unless    => "${alt_command} --display ${title} | /bin/grep ${full_version} | /bin/grep 'priority ${priority}$'",
+      path      => '/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin',
+      logoutput => true,
+      user      => $user,
+      group     => $group,
+    }
   }
 }
